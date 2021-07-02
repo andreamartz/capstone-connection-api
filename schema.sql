944fa18ecd5532e662e2576e99661afd1aefabcf
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-01 21:00:28 -0500

    Add database schema SQL

---
+++ schema.sql
@@ -0,0 +1,63 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(25) UNIQUE,
+  password TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT CHECK (position('@' IN email) > 1) UNIQUE,
+  bio TEXT,
+  photo_url TEXT NOT NULL,
+  portfolio_url TEXT,
+  github_url TEXT,
+  is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE projects (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL UNIQUE,
+  creator_id INTEGER REFERENCES users ON DELETE CASCADE,
+  image TEXT DEFAULT './static/default-project-image-pixabay-dev-illustration.png',
+  repo_url TEXT NOT NULL,
+  site_url TEXT NOT NULL,
+  description VARCHAR(500),
+  feedback_request VARCHAR(500),
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  last_modified TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  text VARCHAR(8) NOT NULL UNIQUE
+);
+
+CREATE TABLE project_likes (
+  id SERIAL PRIMARY KEY,
+  liker_id INTEGER REFERENCES users ON DELETE CASCADE,
+  project_id INTEGER REFERENCES projects ON DELETE CASCADE,
+  UNIQUE (liker_id, project_id)
+);
+
+CREATE TABLE project_comments (
+  id SERIAL PRIMARY KEY,
+  commenter_id INTEGER REFERENCES users ON DELETE CASCADE,
+  project_id INTEGER REFERENCES projects ON DELETE CASCADE,
+  comment TEXT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  last_modified TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE project_comment_likes (
+  id SERIAL PRIMARY KEY,
+  liker_id INTEGER REFERENCES users ON DELETE CASCADE,
+  comment_id INTEGER REFERENCES project_comments ON DELETE CASCADE,
+  UNIQUE (liker_id, comment_id)
+);
+
+-- projects_tags is a many-to-many join table
+CREATE TABLE projects_tags (
+  id SERIAL PRIMARY KEY,
+  project_id INTEGER REFERENCES projects ON DELETE CASCADE,
+  tag_id INTEGER REFERENCES tags ON DELETE CASCADE,
+  UNIQUE (project_id, tag_id)
+);
+
