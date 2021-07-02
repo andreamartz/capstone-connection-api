038119f0afd245331a0d586275dbe6b76ed7d32a
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-01 21:07:44 -0500

    Add packages and middleware and the 'use strict' mode

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+"use strict"
+
 /** Express app for Capstone Connections */
 
 const express = require('express');
+const cors = require("cors");
+
+const { NotFoundError } = require("./expressError");
+const { authenticateJWT } = require("./middleware/auth");
+const authRoutes = require("./routes/auth");
+const usersRoutes = require("./routes/users");
+const projectsRoutes = require("./routes/projects");
+// const tagsRoutes = require("./routes/tags");
+
+const morgan = require("morgan");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authenticateJWT);
+
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+app.use("/projects", projectsRoutes);
+// app.use("/tags", tagsRoutes);
 
 /** 404 handler */
 
