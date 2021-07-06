9525d7c6958d1bdee8468dc3e76d9f4d96d12286
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-06 17:47:12 -0500

    Add data to seed file

--- seed.sql
+++ seed.sql
@@ -47,50 +47,75 @@ VALUES ('testuser',
 INSERT INTO projects (name, creator_id, image, repo_url, site_url, description, feedback_request)
 VALUES ('This is Test Project #1 - One Layover',
         1,
-        'https://via.placeholder.com/300x300?text=p1+image',
+        'https://placekitten.com/300/300',
         'https://github.com/JAWeiss89/onelayover',
         'https://www.onelayover.com/',
         'A place for flight attendants to share layover info',
-        'Please give feedback on frontend and backend.'),
+        'Please give feedback on frontend and backend. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nam reiciendis facere quasi magni iste corporis at placeat ipsam sequi in, voluptas repellendus, recusandae '),
        ('This is Test Project #2 - Access Academy',
         2,
-        'https://via.placeholder.com/300x300?text=p2+image',
+        'https://placem.at/things?w=300',
         'https://github.com/andreamartz/sb-29-capstone-1',
         'https://access-academy.herokuapp.com/',
         'Course creation site for free self-teaching',
-        'Is there a better way to organize my CSS?'),
+        'Is there a better way to organize my CSS? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nam reiciendis facere quasi magni iste corporis at placeat ipsam sequi in, voluptas repellendus, recusandae inventore ratione rerum eaque! Atque earum repellendus nostrum perferendis natus ipsa hic sequi cupiditate quo perspiciatis laboriosam incidunt facilis, laudantium corrupti fugiat voluptatibus et excepturi! Porro illum ullam reiciendis impedit ab quisquam fuga, omnis soluta veniam odit ipsum odio corrupti optio quis neque! Provident nesciunt doloremque iste voluptatum voluptas! Distinctio, voluptatibus sequi qui ratione quam iusto esse dicta sunt! Quod magnam, corrupti, illo perferendis velit dolorem voluptates nam ratione molestiae eaque amet optio tenetur temporibus minima.'),
        ('This is Test Project #3 - Jobly',
         3,
         'https://via.placeholder.com/300x300?text=p3+image',
         'https://github.com/andreamartz/sb-43-React-Jobly-frontend',
         'https://jobs-app.surge.sh/',
         'Jobs and Companies',
-        'Is there a better way to organize my CSS?');
+        'Is there a better way to organize my CSS? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nam reiciendis facere quasi magni iste corporis at placeat ipsam sequi in, voluptas repellendus, recusandae inventore ratione rerum eaque! Atque earum repellendus nostrum '),
+       ('This is Test Project #4 - Allytweetfrontend',
+        4,
+        'https://via.placeholder.com/300x300?text=p4+image',
+        'https://github.com/Cerchie/a11ytweetfrontend/tree/main/src',
+        'https://luciacerchie.dev/',
+        'A11y Retweet Bot',
+        'Atque earum repellendus nostrum perferendis natus ipsa hic sequi cupiditate quo perspiciatis laboriosam incidunt facilis, laudantium corrupti fugiat voluptatibus et excepturi! Porro illum ullam reiciendis impedit ab quisquam fuga, omnis soluta veniam odit ipsum odio corrupti optio quis neque! Provident nesciunt doloremque iste voluptatum voluptas! Distinctio, voluptatibus sequi qui ratione quam iusto esse dicta sunt! Quod magnam, corrupti, illo perferendis velit dolorem voluptates nam ratione molestiae eaque amet optio tenetur temporibus minima.'),
+        ('This is Test Project #5 - My Minimony',
+        2,
+        'https://placem.at/things?w=300',
+        'https://github.com/agiletkiewicz/my-minimony-frontend',
+        'https://www.myminimonyapp.com/',
+        'My Minimony',
+        'Distinctio, voluptatibus sequi qui ratione quam iusto esse dicta sunt!'
+        );
 
-INSERT INTO tags (text)
-VALUES ('HTML'),
-        ('CSS'),
-        ('JS'),
-        ('API');
+INSERT INTO tags (id, text)
+VALUES (1, 'HTML'),
+        (2, 'CSS'),
+        (3, 'JS'),
+        (4, 'API');
 
-INSERT INTO projects_tags (project_id, tag_id)
-VALUES (1, 1),
-       (1, 2),
-       (1, 3),
-       (1, 4),
-       (2, 3),
-       (2, 4);
+INSERT INTO projects_tags (id, project_id, tag_id)
+VALUES (1, 1, 1),
+       (2, 1, 2),
+       (3, 1, 3),
+       (4, 1, 4),
+       (5, 2, 3),
+       (6, 2, 4),
+       (7, 3, 2),
+       (8, 4, 1),
+       (9, 4, 2),
+       (10, 4, 3),
+       (11, 4, 4);
 
 INSERT INTO project_likes (liker_id, project_id)
 VALUES (1, 2),
        (1, 3),
-       (2, 3);
+       (2, 3),
+       (3, 1),
+       (4, 2),
+       (4, 3);
 
 INSERT INTO project_comments (commenter_id, project_id, comment)
-VALUES (1, 2, 'Your whole project is just great!'),
+VALUES (
+        1, 2, 'Your whole project is just great! Atque earum repellendus nostrum perferendis natus ipsa hic sequi cupiditate quo perspiciatis laboriosam incidunt facilis'),
        (2, 1, 'Nice use of code!'),
-       (3, 1, 'Hey, great job!'),
-       (3, 2, 'Way to go!');
+       (3, 1, 'Hey, great job! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nam reiciendis facere quasi magni iste corporis at placeat ipsam sequi in, voluptas repellendus, recusandae inventore ratione rerum eaque! Atque earum repellendus nostrum '),
+       (3, 2, 'Way to go! Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+);
 
 INSERT INTO project_comment_likes (liker_id, comment_id)
 VALUES (2, 1),
