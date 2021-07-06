e08ec572bf1a0e44a4564a78c2d12e8d8d74c3b0
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-06 17:56:42 -0500

    Add cloudinary config info

---
+++ utils/cloudinary.js
@@ -0,0 +1,14 @@
+// require('dotenv').config();
+const config = require('../config');
+
+const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET,CLOUDINARY_NAME } = require("../config");
+
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: CLOUDINARY_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+});
+
+module.exports = { cloudinary };
