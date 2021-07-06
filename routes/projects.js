f62cf470f77ecb739bb6f6ea8c9dd2ea82f2ce6e
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-06 17:55:28 -0500

    Add route to create a new project

--- routes/projects.js
+++ routes/projects.js
@@ -7,17 +7,15 @@ const express = require("express");
 
 const { BadRequestError } = require("../expressError");
 const { ensureAdmin } = require("../middleware/auth");
+const imageUpload = require("../helpers/imageUpload");
 const Project = require("../models/project");
-
+const { cloudinary } = require('../utils/cloudinary');
 // Data validation schemas
-
-
-
 
 
 const router = new express.Router();
 
-/** POST / { project }
+/** POST /projects/new { project }
  * 
  * Purpose: create a new project and save to database
  * 
@@ -31,6 +29,40 @@ const router = new express.Router();
  * 
  * Errors: 
  */
+router.post("/new", async function (req, res, next) {
+  try {
+    // console.log("BACKEND DATA SENT: ", req.body.data);
+    console.log("BACKEND REQ.BODY: ", req.body);
+
+    const fileStr = req.body.image;  // the string representation of the image coming from the frontend
+    
+    const imageData = await imageUpload(fileStr);
+    const image = imageData.secure_url;
+    console.log("IMAGE AFTER UPLOAD: ", image);
+    req.body.image = image;
+    
+    console.log("REQ.BODY: ", req.body);
+    // console.log("REQ.BODY: ", req.body);
+    // // console.log("FILESTR: ", fileStr);
+
+    // const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+    //   upload_preset: 'capstone_connections',
+    // });
+    // console.log("UPLOADRESPONSE: ", uploadResponse);
+    // res.json({ msg: 'yaya' });
+    
+    const project = await Project.create(req.body);
+    return res.status(201).json({ project });
+    
+
+
+
+  } catch (err) {
+    // console.error(err);
+    // res.status(500).json({ err: 'Something went wrong' });
+    return next(err);
+  }
+});
 
 
 
