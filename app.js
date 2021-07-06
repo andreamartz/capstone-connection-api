a480c10947211a7858e17d4a06eec9cd81491625
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-06 17:36:56 -0500

    Edit middleware to limit the size of file upload

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));  // limits file upload size
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
 
