d2f03144017a53595587c9671b9b934b34cec5c0
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-10 14:14:52 -0500

    Add route to update a specific user

--- routes/users.js
+++ routes/users.js
@@ -34,5 +34,16 @@ router.get("/:username", async function (req, res, next) {
   }
 });
 
+/** PATCH /[username] */
+router.patch("/:username", async function (req, res, next) {
+  try {
+    const user = await User.update(req.params.username, req.body);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[username] */
 
 module.exports = router;
