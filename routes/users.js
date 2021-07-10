49a833674997a7f41c31eb3c236e01aab384e492
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-10 14:14:01 -0500

    Add route to get all users

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,17 @@ const router = express.Router();
 
 /** POST / { user } */
 
+/** GET / */
+router.get("/", async function (req, res, next) {
+  try {
+    const users = await User.findAll();
+    return res.json({ users });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[username] */
 
 
 module.exports = router;
