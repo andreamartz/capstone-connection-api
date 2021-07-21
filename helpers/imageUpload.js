3c88c40debc46b42b2679c4e79b9c9632752e9fa
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-21 14:38:38 -0500

    Edit imageUpload.js

--- helpers/imageUpload.js
+++ helpers/imageUpload.js
@@ -1,12 +1,24 @@
 const { cloudinary } = require('../utils/cloudinary');
 
+// Transformations for rounded avatar images:
+// { width: 50, height: 50, crop: 'thumb', radius: 'max', gravity: 'face' }
+// make the extension png to get transparent background
+
 /** Upload image file to Cloudinary */
 const imageUpload = async (fileStr) => {
-  const uploadResponse = await cloudinary.uploader.upload(fileStr, {
-    upload_preset: 'capstone_connections',
-  }); 
-  console.log("UPLOADRESPONSE: ", uploadResponse);
-  return uploadResponse;
+  try {
+    console.log("IN IMAGEUPLOAD", fileStr.substr(0, 40));
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: 'capstone_connections',
+    })
+    .catch(err => {
+      console.log("UPLOAD ERROR:", err)
+    });
+    console.log("UPLOADRESPONSE: ", uploadResponse);
+    return uploadResponse;
+  } catch(error) {
+    console.log("TRY/CATCH ERROR: ", error);
+  }
 }
 
 module.exports = imageUpload;
