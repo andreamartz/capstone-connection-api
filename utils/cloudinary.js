233597356ceb0ad0c4918701a5279ea34e9fb585
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-21 14:08:30 -0500

    Clean up cloudinary configuration file cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,14 +1,9 @@
-// require('dotenv').config();
-const config = require('../config');
-
-const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET,CLOUDINARY_NAME } = require("../config");
-
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: CLOUDINARY_NAME,
-    api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = { cloudinary };
