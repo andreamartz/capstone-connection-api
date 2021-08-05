1aa200992f75a9e2f90633ecb467b941a64cf360
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-05 15:34:23 -0500

    Add method on project_comment model to update a comment

--- models/project_comment.js
+++ models/project_comment.js
@@ -34,6 +34,38 @@ class Project_Comment {
     return project_comment;
   }
 
+  static async update(commentId, data) {
+    const { setCols, values } = sqlForPartialUpdate(
+      data,
+      {
+        commenterId: "commenter_id",
+        projectId: "project_id"
+      }
+    );
+
+    const commentIdVarIdx = "$" + (values.length + 1);
+
+    const querySql = `
+      UPDATE project_comments
+      SET ${setCols}
+      WHERE id = ${commentIdVarIdx}
+      RETURNING
+        id, 
+        commenter_id AS "commenterId",
+        project_id AS "projectId",
+        comment, 
+        created_at AS "createdAt", 
+        last_modified AS "lastModified"
+    `;
+
+    const result = await db.query(querySql, [...values, commentId]);
+    const comment = result.rows[0];
+
+    if (!comment) throw new NotFoundError(`No comment found with id ${userId}`);
+
+    return comment;
+  }
+
 }
 
 module.exports = Project_Comment;
