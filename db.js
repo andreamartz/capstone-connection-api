1a4649cf09af9d0646c90dff4217644abf05a35e
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-15 17:32:14 -0500

    Update db.js for local server

--- db.js
+++ db.js
@@ -7,19 +7,19 @@ const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
 // USE FOR LOCAL SERVER
-// const db = new Client({
-//   connectionString: DB_URI
-// });
-
-// USE FOR DEPLOYMENT; also, you need to add to .env file the DATABASE_URL of the deployed database (and NODE_ENV??)
 const db = new Client({
-  connectionString: DB_URI,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: DB_URI
 });
 
-console.log("DB_URI: ", DB_URI);
+// USE FOR DEPLOYMENT; also, you need to add to .env file the DATABASE_URL of the deployed database (and NODE_ENV??)
+// const db = new Client({
+//   connectionString: DB_URI,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+
 db.connect();
 
 module.exports = db;
