05ed0bc036544e026e3f5c3ed2b2a00ea6ca7595
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-02 15:09:11 -0500

    Improve code formatting consistency in projectsSqlToExpress.js

--- helpers/projectsSqlToExpress.js
+++ helpers/projectsSqlToExpress.js
@@ -1,103 +1,139 @@
-"use strict";
+'use strict';
 
 const _ = require('lodash');
 
 /** fromDbToExpress
- * 
- * Input: prjRows
+ *
+ *  Input: array of project objects with lots of duplicated info
+ *
+ *  prjRows will look like this:
  *   {
  *     '1': [{...}, {...}],
  *     '2': [{...}, {...}],
  *   }
- *  
+ *
  *   * Each property in prjRows is a project id, and each value is an array of project objects.
- *   * Most of the data in each project array is duplicated in each project object. 
+ *   * Most of the data in each project array is duplicated in each project object.
  *   *This happens because there can be multiple tags, multiple comments, and multiple likes associated with a single project.
- * 
+ *
  * Purpose: fromDbToExpress reduces each project array down to a single object and pushes it onto a new array, called 'projects', which is returned from the function.
+ *
+ * Returns: an array of individual project objects
  */
 
 const projectsSqlToExpress = (results, currentUserId) => {
   const projects = [];
   // console.log("RESULTS FROM project model: ", results);
   // Group results data by project id
-  const prjRows = _.groupBy(results.rows, row => row.id);
+  const prjRows = _.groupBy(results.rows, (row) => row.id);
 
-  /** 
+  /**
    * Reduce each project array down to a single project object.
-  */ 
+   */
 
   for (const prop in prjRows) {
-    let prjRow = prjRows[prop].reduce((accumulator, data) => {
-      const { id, name, image, repoUrl, siteUrl, description, feedbackRequest, createdAt, lastModified, tagId, tagText, likeId, likerUserId, prjCommentsCount, creatorId, firstName, lastName, photoUrl } = data;
+    let prjRow = prjRows[prop].reduce(
+      (accumulator, data) => {
+        const {
+          id,
+          name,
+          image,
+          repoUrl,
+          siteUrl,
+          description,
+          feedbackRequest,
+          createdAt,
+          lastModified,
+          tagId,
+          tagText,
+          likeId,
+          likerUserId,
+          prjCommentsCount,
+          creatorId,
+          firstName,
+          lastName,
+          photoUrl,
+        } = data;
 
-      // Create record containing project-level data
-      const newRecord = {id, name, image, repoUrl, siteUrl, description, feedbackRequest, createdAt, lastModified, prjCommentsCount: +prjCommentsCount};
-      
-      // Store project creator data in an object
-      newRecord.creator = { 
-        id: creatorId,
-        firstName, 
-        lastName, 
-        photoUrl
-      };
+        // Create record containing project-level data
+        const newRecord = {
+          id,
+          name,
+          image,
+          repoUrl,
+          siteUrl,
+          description,
+          feedbackRequest,
+          createdAt,
+          lastModified,
+          prjCommentsCount: +prjCommentsCount,
+        };
 
-      // Store project tags data in an array
-      if (tagId) {
-        newRecord.tags = [...accumulator.tags, {id: tagId, text: tagText}];
+        // Store project creator data in an object
+        newRecord.creator = {
+          id: creatorId,
+          firstName,
+          lastName,
+          photoUrl,
+        };
+
+        // Store project tags data in an array
+        if (tagId) {
+          newRecord.tags = [...accumulator.tags, { id: tagId, text: tagText }];
+        }
+
+        // Store project likes data in an array
+        if (likeId) {
+          newRecord.likes = [...accumulator.likes, { likeId, likerUserId }];
+        }
+
+        return newRecord;
+      },
+      {
+        id: +prop,
+        name: '',
+        image: '',
+        repoUrl: '',
+        siteUrl: '',
+        description: '',
+        feedbackRequest: '',
+        createdAt: '',
+        lastModified: '',
+        prjCommentsCount: null,
+        creator: {},
+        tags: [],
+        likes: [],
       }
-      
-      // Store project likes data in an array
-      if (likeId) {
-        newRecord.likes = [...accumulator.likes, {likeId, likerUserId}];
-      }
-
-      return newRecord;
-
-    }, { id: +prop, 
-          name: "",
-          image: "", 
-          repoUrl: "", 
-          siteUrl: "", 
-          description: "", 
-          feedbackRequest: "", 
-          createdAt: "", 
-          lastModified: "", 
-          prjCommentsCount: null,
-          creator: {}, 
-          tags: [],
-          likes: []
-    });
+    );
     projects.push(prjRow);
-  };
+  }
 
-  /** 
+  /**
    * Eliminate duplicates in tags and likes arrays on each project.
-  */ 
-   for (const project of projects) {
+   */
+  for (const project of projects) {
     // create array of unique tags
-    const uniqTags = _.uniqBy(project.tags, function(tag){
+    const uniqTags = _.uniqBy(project.tags, function (tag) {
       return tag.id;
     });
     project.tags = uniqTags;
 
     // create array of unique likes
-    const uniqLikes = _.uniqBy(project.likes, function(like) {
+    const uniqLikes = _.uniqBy(project.likes, function (like) {
       return like.likeId;
     });
     project.likesCount = uniqLikes.length;
-    
+
     // likedByCurrentUser is equal to a like object if the currentUser has liked the project; otherwise it is undefined
-    const likedByCurrentUser = uniqLikes
-      .find(like => like.likerUserId === currentUserId)
-    ;
-    
+    const likedByCurrentUser = uniqLikes.find(
+      (like) => like.likerUserId === currentUserId
+    );
     project.currentUsersLikeId = likedByCurrentUser
-    ? likedByCurrentUser.likeId
-    : null;
+      ? likedByCurrentUser.likeId
+      : null;
   }
-  // console.log("PROJECTS: ", projects);
+  console.log('PROJECTS FROM PROJECTSSQLTOEXPRESS: ', projects);
   return projects;
-}
+};
 
 module.exports = { projectsSqlToExpress };
