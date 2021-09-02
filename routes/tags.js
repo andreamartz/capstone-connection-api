852298318db6222813114aabf95ce3740b932176
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-02 15:11:20 -0500

    Improve code formatting consistency in tags routes file

--- routes/tags.js
+++ routes/tags.js
@@ -1,34 +1,33 @@
-"use strict";
+'use strict';
 
 /** Routes for tags */
 
-const jsonschema = require("jsonschema");
-const express = require("express");
+const jsonschema = require('jsonschema');
+const express = require('express');
 
-const { BadRequestError } = require("../expressError");
-const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
-const imageUpload = require("../helpers/imageUpload");
-const Tag = require("../models/tag");
+const { BadRequestError } = require('../expressError');
+const { ensureLoggedIn, ensureAdmin } = require('../middleware/auth');
+const imageUpload = require('../helpers/imageUpload');
+const Tag = require('../models/tag');
 
 // Data validation schemas
-
 
 const router = new express.Router();
 
 /** POST /tags
  * Purpose: create a new tag and save to database
- * 
+ *
  * Req body contains the text of the tag: { text: 'TAGTEXT' }
- * 
+ *
  * Returns: { tag: { id, text }}
- * 
+ *
  * Auth required: User must be an admin
- * 
+ *
  * Errors:
- * 
+ *
  */
-router.post("/", ensureAdmin, async function(req, res, next) {
-  console.debug("CREATE NEW TAG");
+router.post('/', ensureAdmin, async function (req, res, next) {
+  console.debug('CREATE NEW TAG');
   try {
     const tag = await Tag.create(req.body);
     return res.status(201).json({ tag });
@@ -37,20 +36,20 @@ router.post("/", ensureAdmin, async function(req, res, next) {
   }
 });
 
-/** GET / 
- * 
+/** GET /
+ *
  * Purpose: retrieve all tags from the database
- * 
+ *
  * Req body: none
- * 
- * Returns: 
- * 
- * Auth required: 
- * 
- * Errors: 
+ *
+ * Returns:
+ *
+ * Auth required:
+ *
+ * Errors:
  */
 
-router.get('/', ensureLoggedIn, async function(req, res, next) {
+router.get('/', ensureLoggedIn, async function (req, res, next) {
   try {
     const tags = await Tag.getAll(req.query);
     return res.json({ tags });
