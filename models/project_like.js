2bc6c44cefffb8c876e178a03b3ba8362ff0fcf1
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-05 15:25:51 -0500

    Improve code formatting

--- models/project_like.js
+++ models/project_like.js
@@ -1,29 +1,29 @@
-"use strict";
+'use strict';
 
-const db = require("../db");
+const db = require('../db');
 const {
   BadRequestError,
   UnauthorizedError,
   ForbiddenError,
   NotFoundError,
-} = require("../expressError");
-const { sqlForPartialUpdate } = require("../helpers/sql");
+} = require('../expressError');
+const { sqlForPartialUpdate } = require('../helpers/sql');
 
 /** Functions for project_likes */
 
 class Project_Like {
-  /** Purpose: to create a new like for a project, update database, return new project like data 
-   * 
+  /** Purpose: to create a new like for a project, update database, return new project like data
+   *
    * Input: {
    *   likerId,
    *   projectId
    * }
-   * 
-   * Returns: 
-   * 
+   *
+   * Returns:
+   *
    * Errors(s):
-  */
-  
+   */
+
   static async create(data) {
     let query = `
       INSERT INTO project_likes (
@@ -34,26 +34,26 @@ class Project_Like {
       RETURNING id, liker_id AS "likerId",
         project_id AS "projectId"
     `;
-    const likeRes = await db.query(query, [
-      data.likerId, 
-      data.projectId
-    ]);
+    const likeRes = await db.query(query, [data.likerId, data.projectId]);
 
     const like = likeRes.rows[0];
-    console.log("LIKE: ", like);
+    console.log('LIKE: ', like);
+
+    return like;
+  }
 
     return like;
   }
 
   /** Purpose: to delete a like from a project in database
-   * 
+   *
    * Input: { id }
-   * 
-   * Returns: 
-   * 
-   * Errors(s): Throws a NotFoundError if the like is not found
-  */
-  
+   *
+   * Returns:
+   *
+   * Error(s): Throws a NotFoundError if the like is not found
+   */
+
   static async remove(id) {
     const query = `
       DELETE
