0f645f5a8d254db0fdd7ef7141738fb579187fcb
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-07 13:15:02 -0500

    Change db settings to update deployment

--- db.js
+++ db.js
@@ -7,17 +7,17 @@ const { Client } = require('pg');
 const { DB_URI } = require('./config');
 
 // USE FOR LOCAL SERVER
-const db = new Client({
-	connectionString: DB_URI,
-});
+// const db = new Client({
+// 	connectionString: DB_URI,
+// });
 
 // USE FOR DEPLOYMENT; also, you need to add to .env file the DATABASE_URL of the deployed database (and NODE_ENV??)
-// const db = new Client({
-//   connectionString: DB_URI,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+const db = new Client({
+	connectionString: DB_URI,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
 
 db.connect();
 
